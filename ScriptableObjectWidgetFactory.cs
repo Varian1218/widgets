2382f26934da133c7b9b5400dd3bf93eec1b67fe
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-03-31 22:57:46 +0700

    1.1.0

---
+++ ScriptableObjectWidgetFactory.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Widgets
+{
+    [CreateAssetMenu(menuName = "Widgets/Widget Factory", fileName = "Widget Factory", order = 1)]
+    public class ScriptableObjectWidgetFactory : ScriptableObject, IWidgetFactory
+    {
+        private IWidgetFactory _impl;
+
+        public IWidgetFactory Impl
+        {
+            set => _impl = value;
+        }
+
+        public T CreateWidget<T>(string widgetName) where T : class
+        {
+            return _impl.CreateWidget<T>(widgetName);
+        }
+
+        public void DestroyWidget(IWidget widget)
+        {
+            _impl.DestroyWidget(widget);
+        }
+    }
+}
