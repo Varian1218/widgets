cd6a87ea276f09f245cbad06510a38a5567384f9
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-02 18:56:24 +0700

    1.2.4

---
+++ LoadWidgetsArrayAction.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Widgets
+{
+    public class LoadWidgetsArrayAction : MonoBehaviour
+    {
+        [SerializeField] private UnityWidgets[] widgets;
+        [SerializeField] private ScriptableObjectWidgetsArray widgetsArray;
+
+        public void Invoke()
+        {
+            widgetsArray.Impl = new WidgetsArray
+            {
+                Widgets = widgets.Select(it => it as IWidgets).ToArray()
+            };
+            foreach (var it in widgets)
+            {
+                DontDestroyOnLoad(it);
+            }
+        }
+    }
+}
