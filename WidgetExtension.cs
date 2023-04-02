489ce7b2c3b190f85ff65d17a4f67b5a841d0c22
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-02 14:49:54 +0700

    1.1.4

---
+++ WidgetExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace Widgets
+{
+    public static class WidgetExtension
+    {
+        public static void Add(this IWidgets widgets, IWidget widget)
+        {
+            widgets.Add(widget, widget.InstanceId);
+        }
+    }
+}
