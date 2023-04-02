6d6d5da16b9f44c17768a0a961c32cf3757bf1c6
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-02 18:14:37 +0700

    1.2.3

---
+++ ScriptableObjectLoadWidgetFactory.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Widgets
+{
+    [CreateAssetMenu(menuName = "Widgets/Load Widget Factory", fileName = "Load Widget Factory", order = 1)]
+    public class ScriptableObjectLoadWidgetFactory : ScriptableObject
+    {
+        [SerializeField] private WidgetDatabase widgetDatabase;
+        [SerializeField] private ScriptableObjectWidgetFactory widgetFactory;
+
+        public void Invoke()
+        {
+            var wf = new WidgetFactory();
+            widgetFactory.Impl = wf;
+            foreach (var (hash, widget) in widgetDatabase)
+            {
+                wf.AddWidget(hash, widget);
+            }
+        }
+    }
+}
