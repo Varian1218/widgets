74827d9eb6757d7535763efde3f31a3934a86995
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-02 17:27:30 +0700

    1.2.2

--- ScriptableObjectWidgetsArray.cs
+++ ScriptableObjectWidgetsArray.cs
@@ -6,9 +6,14 @@ namespace Widgets
     [CreateAssetMenu(menuName = "Widgets/Widgets Array", fileName = "Widgets Array", order = 1)]
     public class ScriptableObjectWidgetsArray : ScriptableObject, IWidgetsArray
     {
-        [SerializeField] private UnityWidgets[] widgets;
+        private IWidgetsArray _impl;
 
-        public IWidgets this[int index] => widgets[index];
-        public IWidgets this[Index index] => widgets[index];
+        public IWidgetsArray Impl
+        {
+            set => _impl = value;
+        }
+
+        public IWidgets this[int index] => _impl[index];
+        public IWidgets this[Index index] => _impl[index];
     }
 }
