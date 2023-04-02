613add7ad7abfd2074ef4572b5db0a0220e6a1ac
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-02 15:40:44 +0700

    1.2.0

---
+++ IWidgetsArray.cs
@@ -0,0 +1,7 @@
+﻿namespace Widgets
+{
+    public interface IWidgetsArray
+    {
+        IWidgets this[int index] { get; }
+    }
+}
---
+++ ScriptableObjectWidgetsArray.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Widgets
+{
+    [CreateAssetMenu(menuName = "Widgets/Widgets Array", fileName = "Widgets Array", order = 1)]
+    public class ScriptableObjectWidgetsArray : ScriptableObject, IWidgetsArray
+    {
+        [SerializeField] private UnityWidgets[] widgets;
+
+        public IWidgets this[int index] => widgets[index];
+    }
+}
