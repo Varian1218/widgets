711e1d2e02629ea080844028e0c0a68d7fbe5d70
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-03 21:29:53 +0700

    1.3.1

---
+++ LoadCanvasAction.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Widgets
+{
+    public class LoadCanvasAction : MonoBehaviour
+    {
+        [SerializeField] private UnityCanvas canvas;
+        [SerializeField] private ScriptableObjectCanvas sCanvas;
+
+        public void Invoke()
+        {
+            sCanvas.Impl = canvas;
+        }
+    }
+}
