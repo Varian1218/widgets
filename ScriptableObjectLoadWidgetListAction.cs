b7e88641f373c2d4d5021f5853b9364b8ce81d53
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-08 22:19:23 +0700

    1.11.0

---
+++ ScriptableObjectLoadWidgetListAction.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Widgets
+{
+    [CreateAssetMenu(fileName = "Load Widget List Action", menuName = "Widgets/Load Widget List Action", order = 1)]
+    public class ScriptableObjectLoadWidgetListAction : ScriptableObject
+    {
+        [SerializeField] private ScriptableObjectWidgetList widgetList;
+
+        public void Invoke()
+        {
+            widgetList.Impl = new WidgetList();
+        }
+    }
+}
