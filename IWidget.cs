0a6c2e16b38e404f1276faec62dd50cde85bf600
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-23 11:41:02 +0700

    1.15.0

--- IWidget.cs
+++ IWidget.cs
@@ -3,15 +3,13 @@ using Transforms;
 
 namespace Widgets
 {
-    public interface IWidget
+    public interface IWidget : IComponentGetHandler
     {
         event Action Hidden;
         int Index { set; }
         int InstanceId { get; }
-        ITransform Parent { set; }
         event Action Showed;
         bool Visible { set; }
         void Destroy();
-        T GetComponent<T>();
     }
 }
